3a264773e99583ef876feb7d15fabf3efc65a7dd
xuyongtao <xuyongtao2009@gmail.com>
2017-12-26 16:56:37 +0800

    no message

--- server.js
+++ server.js
@@ -4,15 +4,50 @@ var configs = {
   token: "mytoken"
 };
 
+var menus = {
+  button: [
+    {
+      type: "click",
+      name: "今日歌曲",
+      key: "V1001_TODAY_MUSIC"
+    },
+    {
+      name: "菜单",
+      sub_button: [
+        {
+          type: "view",
+          name: "搜索",
+          url: "http://www.soso.com/"
+        },
+        {
+          type: "click",
+          name: "赞一下我们",
+          key: "V1001_GOOD"
+        }
+      ]
+    }
+  ]
+};
+
 var express = require("express");
 var wechat = require("wechat");
+var wechatApi = require("wechat-api");
+var api = new wechatApi(configs.appID, configs.appsecret);
 var app = express();
 var port = "80";
 
 app.get("/test", function(req, res) {
   res.send("hello world");
 });
-app.use("/wechat", wechat("token", function(req, res, next) {}));
+app.use(
+  "/wechat",
+  wechat("token", function(req, res, next) {
+    api.createMenu(menu, function() {
+      console.log(arguments);
+    });
+  })
+);
+app.use("/reply");
 app.listen(port, "0.0.0.0", err => {
   if (err) {
     console.error(err);
